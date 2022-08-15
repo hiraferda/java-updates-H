d5f03579b1823d67789a61f5341124f08abfaaac
hiraferda <hirayavuz23ferda@gmail.com>
2022-08-15 11:16:50 -0400

    SwappingArray class is created and the task is completed

---
+++ m2-enum-h/src/main/java/com/HW/SwappingArray.java
@@ -0,0 +1,15 @@
+package com.HW;
+
+public class SwappingArray<T> {
+
+    public static <T> T[] swap(T[] array, int index, int index2){
+        T temp = array[index];
+        array[index] = array[index2];
+        array[index2] = temp;
+        return array;
+    }
+}
+/*
+Write a generic method to exchange the positions of two different elements in
+array.
+ */
